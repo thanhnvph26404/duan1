338898569fbaca0d36ad2aa940aa8752edeaa172
thanhnvph26404 <thanhnvph26404@gmail.com>
2022-11-01 08:51:37 +0700

    test

---
+++ admin/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quản trị website</title>
+    <link rel="stylesheet" href="../view/css/style.css">
+    
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1>QUẢN TRỊ WEBSITE</h1>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Trang chủ</a></li>
+                    <li><a href="index.php?act=adddm">Loại hàng</a></li>
+                    <li><a href="index.php?act=addsp">Hàng hóa</a></li>
+                    <li><a href="index.php?act=dskh">Khách hàng</a></li>
+                    <li><a href="index.php?act=dsbl">Bình luận</a></li>
+                    <li><a href="index.php?act=listbill">Danh sách đơn hàng</a></li>
+                    <li><a href="index.php?act=thongke">Thống kê</a></li>
+                </ul>
+            </nav>
+        </header>
